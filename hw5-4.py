121c6ce7571ac985c451737fda90a7fa927d68f9
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-11-03 09:55:54 -0400

    add file hw5-4

---
+++ hw5-4.py
@@ -0,0 +1,32 @@
+#Author:JTI 11/2/21
+
+#Question 1
+print("Python"[2])
+
+
+#Question 2
+print("Strings are simply a sequence of characters."[30])
+
+#Question 3 
+print(len("exellent"))
+
+#Question 4
+print("apple" in "Pineapple")
+
+#Question 5
+print("Peach" > "pearl")
+
+# Question 6
+print("x" * 100)
+
+# QUestion 7
+print(str(54321))
+
+# Question 8
+print("p" not in "Pineapple")
+
+# Question 9
+print("\"livin' on a prayer\" is my favorite song.")
+
+# Question 10
+print(len("\"livin' on a prayer\" is my favorite song."))
